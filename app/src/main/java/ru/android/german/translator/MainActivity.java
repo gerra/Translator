ddffe25024fd9bf999809e799e615f2d89215697
warrior_c <gerralizza@gmail.com>
2014-09-27 16:54:03 +0400

    initial commit

---
+++ app/src/main/java/ru/android/german/translator/MainActivity.java
@@ -0,0 +1,30 @@
+package ru.android.german.translator;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+/**
+ * Created by german on 26.09.14.
+ */
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_layout);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+
+}
