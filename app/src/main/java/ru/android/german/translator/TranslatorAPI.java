850a85eb6dd08850ce824d203397ce77b82a1a9c
creed <alexchekmenev@mail.ru>
2014-09-28 14:25:48 +0400

    TranslatorAPI.java added

--- app/src/main/java/ru/android/german/translator/TranslatorAPI.java
+++ app/src/main/java/ru/android/german/translator/TranslatorAPI.java
@@ -1,7 +1,34 @@
 package ru.android.german.translator;
 
+import android.os.AsyncTask;
+
+import java.util.concurrent.TimeUnit;
+
 /**
  * Created by creed on 28.09.14.
  */
 public class TranslatorAPI {
+
+    public class MyTask extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected void doInBackground(Void ) {
+            try {
+                TimeUnit.SECONDS.sleep(2);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+        }
+    }
 }
